6d3d13ead4932fce378258b4236d166823b4d002
Zaid Nouri <s12217916@stu.najah.edu>
2026-04-13 13:25:31 +0300

    add Refresh Token to increase Security

--- src/Api/AuthAxiosInstance.jsx
+++ src/Api/AuthAxiosInstance.jsx
@@ -1,20 +1,59 @@
 import axios from "axios";
 import i18n from "../i18next";
+import useAuthStore from "../store/useAuthStore";
 
 const AuthAxiosInstance = axios.create({
-    baseURL: 'https://knowledgeshop.runasp.net/api/',
+  baseURL: 'https://knowledgeshop.runasp.net/api/',
+  withCredentials: true,
 });
+
 
 AuthAxiosInstance.interceptors.request.use((config) => {
-    const token = localStorage.getItem("AccessToken");
+  const token = useAuthStore.getState().token;
 
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+  config.headers["Accept-Language"] = i18n.language;
+
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+
+AuthAxiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const refreshResponse = await axios.get(
+          'https://knowledgeshop.runasp.net/api/auth/Account/RefreshToken',
+          { withCredentials: true }
+        );
+
+        const newAccessToken = refreshResponse.data.accessToken;
+
+        useAuthStore.getState().setToken(newAccessToken);
+
+        originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+
+        return AuthAxiosInstance(originalRequest);
+
+      } catch (err) {
+        console.log("Refresh failed");
+
+        useAuthStore.getState().logout(); 
+
+        return Promise.reject(err);
+      }
     }
 
-    config.headers["Accept-Language"] = i18n.language;
-
-    return config;
-});
+    return Promise.reject(error);
+  }
+);
 
 export default AuthAxiosInstance;
